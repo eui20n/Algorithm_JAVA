842946eb70c67582ab5681f017ce4c119d32a7f5
eui20n <eui20n@naver.com>
2022-12-22 19:04:49 +0900

    2022-12-22

--- src/baekjoon/start.java
+++ src/baekjoon/start.java
@@ -5,6 +5,7 @@
  * 시간복잡도 :
  * 핵심 로직 및 생각 :
  * 소요 시간 :
+ * 제출할 때, package 삭제할 것
  * */
 
 package baekjoon;
--- src/baekjoon/t1000f1999/p1000/Main.java
+++ src/baekjoon/t1000f1999/p1000/Main.java
@@ -5,12 +5,34 @@
  * 시간복잡도 : O(1) -> 더하기 연산 한번
  * 핵심 로직 및 생각 : 입력 연습
  * 소요 시간 : 1초
+ * 제출할 때, package 삭제할 것
  * */
 
 package baekjoon.t1000f1999.p1000;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+//        Scanner sc = new Scanner(System.in);
+//
+//        int num1 = sc.nextInt();
+//        int num2 = sc.nextInt();
+//
+//        System.out.println(num1 + num2);
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String temp = bf.readLine();
+        String[] tempArr = temp.split(" ");
+
+        int num1 = Integer.parseInt(tempArr[0]);
+        int num2 = Integer.parseInt(tempArr[1]);
+
+        System.out.println(num1 + num2);
 
     }
 }
