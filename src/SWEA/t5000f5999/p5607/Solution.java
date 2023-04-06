50bbb8112b4059a66016d501b7ea724683d69279
eui20n <eui20n@naver.com>
2023-04-06 23:08:58 +0900

    2023-04-06

--- src/SWEA/t5000f5999/p5607/Solution.java
+++ src/SWEA/t5000f5999/p5607/Solution.java
@@ -18,6 +18,8 @@ import java.util.Arrays;
 
 public class Solution {
     static int T;
+    static long[] fac;
+    static final int MOD = 1234567891;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         T = Integer.parseInt(br.readLine());
@@ -26,6 +28,34 @@ public class Solution {
             int[] num = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             int N = num[0];
             int R = num[1];
+
+            fac = new long[N + 1];
+            fac[1] = 1;
+
+            for (int j = 2; j <= N; j++){
+                fac[j] = (fac[j - 1] * j) % MOD;
+            }
+            long bottomNum = (fac[R] * fac[N - R]) % MOD;
+            bottomNum = perma(bottomNum, MOD - 2);
+            System.out.printf("#%d %d %n", i, (fac[N] * bottomNum) % MOD);
         }
     }
+
+    static long perma(long num, int top) {
+        // 페르마의 소정리
+        if (top == 0)
+            return 1;
+        if (top == 1)
+            return num;
+        if (top % 2 == 0) {
+            long tmp = perma(num, top/2);
+            return (tmp * tmp) % MOD;
+        }
+
+        long tmp = perma(num, top - 1) % MOD;
+        return (tmp * num) % MOD;
+    }
 }
+
+// ?? ㅠㅠ
+// 참고함
