76d0468532388109a52e560faaee9b342a04344f
eui20n <eui20n@naver.com>
2023-04-30 20:51:51 +0900

    2023-04-30

--- src/baekjoon/start.java
+++ src/baekjoon/start.java
@@ -10,8 +10,57 @@
 
 package baekjoon;
 
-public class start {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class start {
+    static int N, M;
+    static List<List<Integer>> info;
+    static int start, end;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] tmp = br.readLine().split(" ");
+        N = Integer.parseInt(tmp[0]);
+        M = Integer.parseInt(tmp[1]);
+
+        info = makeList(N);
+
+        for (int i = 1; i <= M; i++) {
+            String[] tmp2 = br.readLine().split(" ");
+            int floor = Integer.parseInt(tmp2[0]);
+            int addPoint = Integer.parseInt(tmp2[1]);
+
+            for (int j = floor; j <= N; j =  j + addPoint) {
+                info.get(j).add(i);
+            }
+        }
+
+        String[] tmp3 = br.readLine().split(" ");
+        start = Integer.parseInt(tmp3[0]);
+        end = Integer.parseInt(tmp3[1]);
+
+        for (int i = 0; i < info.size(); i++) {
+            if (info.get(i).size() == 0)
+                continue;
+            System.out.println(i);
+            System.out.println(Arrays.toString(info.get(i).toArray()));
+        }
+    }
+
+    static List<List<Integer>> makeList(int size) {
+        List<List<Integer>> tmp = new ArrayList<>();
+        for (int i = 0; i <= size; i++) {
+            tmp.add(new ArrayList<>());
+        }
+        return tmp;
     }
 }
+
+// 자기가 갔던 층은 더 이상 안가도 됨
+// 현재 층에서 갈 수 있는 엘리베이터를 봐야함
+// 현재 엘리베이터가 갈 수 있는 곳을 보는 것이 아니라
--- src/baekjoon/t1000f1999/p1525/Main.java
+++ src/baekjoon/t1000f1999/p1525/Main.java
@@ -14,11 +14,16 @@ package baekjoon.t1000f1999.p1525;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
+
 
 public class Main {
     static int[][] arr = new int[3][3];
     static int[] startPoint;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -30,7 +35,6 @@ public class Main {
                     startPoint = new int[] {i, j};
             }
         }
-        print(arr);
     }
 
     static void print(int[][] arr) {
