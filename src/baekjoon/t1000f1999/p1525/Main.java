5041c19dbbb45ef6d8ea8fa09db50d4e27c68b83
김의년 <kimeuinyun1@gmail.com>
2023-05-01 17:57:07 +0900

    2023-05-01

--- src/baekjoon/t1000f1999/p1525/Main.java
+++ src/baekjoon/t1000f1999/p1525/Main.java
@@ -37,6 +37,41 @@ public class Main {
         }
     }
 
+    static void check() {
+        bfs();
+    }
+
+    static void bfs() {
+        Deque<int[]> q = new ArrayDeque<>();
+        q.add(new int[] {startPoint[0], startPoint[1], 0}); // x y time
+
+        while (true) {
+            if (q.isEmpty())
+                break;
+
+            int[] tmp = q.poll();
+            int x = tmp[0];
+            int y = tmp[1];
+            int time = tmp[2];
+
+            for (int z = 0; z < 4; z++) {
+                int nx = x + dx[z];
+                int ny = y + dy[z];
+
+                if (0 > nx || nx >= 3)
+                    continue;
+                if (0 > ny || ny >= 3)
+                    continue;
+                // 방문 처리 코드
+                // arr의 상태가 바뀜 => 이 부분도 다른 것으로 바뀔 예정
+                arr[x][y] = arr[nx][ny];
+                arr[nx][ny] = 0;
+                q.add(new int[] {nx, ny, time + 1});
+
+            }
+        }
+    }
+
     static void print(int[][] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
@@ -54,3 +89,8 @@ public class Main {
 
 // 이게 순서대로 만들어야하나?
 // 그리고, 이미 만들어진 자리가 확정이 된 퍼즐의 위치는 절대로 바꾸면 안되는 건가?
+
+// 그냥 모든 경우를 다 보기 => 최대 9!(360,000)임
+// 저거는 정말 모든 경우이고, 실제로는 저 만큼은 나올 수 가 없음
+// 큐에 새로운 데이터가 들어가야함 => 현재 상태 배열, 이게 있으면 메모리가 견디질 못함
+// 모든 경우에 대해서 방문처리를 해야하는데, 그러기에는 메모리가 너무 적음 => 이거만 해결하면 됨
