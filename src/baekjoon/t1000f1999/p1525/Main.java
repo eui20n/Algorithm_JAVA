435996a21223e20c3092b156edb26a9535c55d0f
김의년 <kimeuinyun1@gmail.com>
2023-05-03 13:45:57 +0900

    2023-05-03

--- src/baekjoon/t1000f1999/p1525/Main.java
+++ src/baekjoon/t1000f1999/p1525/Main.java
@@ -17,9 +17,21 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
-
+import java.util.HashSet;
 
 public class Main {
+    static class Point {
+        int x, y, time;
+        String arr;
+
+        public Point(int x, int y, int time, String arr) {
+            this.x = x;
+            this.y = y;
+            this.time = time;
+            this.arr = arr;
+        }
+    }
+
     static int[][] arr = new int[3][3];
     static int[] startPoint;
     static int[] dx = {0, 0, -1, 1};
@@ -35,24 +47,31 @@ public class Main {
                     startPoint = new int[] {i, j};
             }
         }
+        check();
+
     }
 
     static void check() {
-        bfs();
+        System.out.println(bfs());
     }
 
-    static void bfs() {
-        Deque<int[]> q = new ArrayDeque<>();
-        q.add(new int[] {startPoint[0], startPoint[1], 0}); // x y time
+    static int bfs() {
+        Deque<Point> q = new ArrayDeque<>();
+        HashSet<String> visited = new HashSet<>();
+        visited.add(arrToString(arr));
+        q.add(new Point(startPoint[0], startPoint[1], 0, arrToString(arr))); // x y time arr
 
         while (true) {
             if (q.isEmpty())
                 break;
 
-            int[] tmp = q.poll();
-            int x = tmp[0];
-            int y = tmp[1];
-            int time = tmp[2];
+            Point tmp = q.poll();
+            int x = tmp.x;
+            int y = tmp.y;
+            int time = tmp.time;
+            String str = tmp.arr;
+
+            arr = stringToArr(str);
 
             for (int z = 0; z < 4; z++) {
                 int nx = x + dx[z];
@@ -62,15 +81,46 @@ public class Main {
                     continue;
                 if (0 > ny || ny >= 3)
                     continue;
+                if (arrToString(arr).equals("123456780"))
+                    return time;
                 // 방문 처리 코드
                 // arr의 상태가 바뀜 => 이 부분도 다른 것으로 바뀔 예정
                 arr[x][y] = arr[nx][ny];
                 arr[nx][ny] = 0;
-                q.add(new int[] {nx, ny, time + 1});
-
+                String newStr = arrToString(arr);
+                if (visited.contains(newStr)) {
+                    arr[nx][ny] = arr[x][y];
+                    arr[x][y] = 0;
+                    continue;
+                }
+                q.add(new Point(nx, ny, time + 1, newStr));
+                visited.add(newStr);
+                arr[nx][ny] = arr[x][y];
+                arr[x][y] = 0;
             }
         }
+        return -1;
     }
+
+     static String arrToString(int[][] arr) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                sb.append(arr[i][j]);
+            }
+        }
+        return sb.toString();
+     }
+
+     static int[][] stringToArr(String str) {
+        int[][] newArr = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                newArr[i][j] = str.charAt(j + 3 * i) - '0';
+            }
+        }
+        return newArr;
+     }
 
     static void print(int[][] arr) {
         for (int i = 0; i < arr.length; i++) {
@@ -82,15 +132,3 @@ public class Main {
         System.out.println();
     }
 }
-
-// 방문처리 가능?
-// 메모리가 상당히 작음 => queue에 함부로 넣으면 안됨
-// 근데 자바는 조금 많이 주네
-
-// 이게 순서대로 만들어야하나?
-// 그리고, 이미 만들어진 자리가 확정이 된 퍼즐의 위치는 절대로 바꾸면 안되는 건가?
-
-// 그냥 모든 경우를 다 보기 => 최대 9!(360,000)임
-// 저거는 정말 모든 경우이고, 실제로는 저 만큼은 나올 수 가 없음
-// 큐에 새로운 데이터가 들어가야함 => 현재 상태 배열, 이게 있으면 메모리가 견디질 못함
-// 모든 경우에 대해서 방문처리를 해야하는데, 그러기에는 메모리가 너무 적음 => 이거만 해결하면 됨
